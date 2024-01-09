73ca314a44fa9d71022bdf1c55606a04ee30020b
Francisco Mejia <franjmr1@gmail.com>
2024-01-09 13:27:43 -0700

    Lista,fetch,css init

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
 
+  const [criptos,setCriptos] = useState()
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}assets`)
+      .then((resp) => resp.json())
+      .then((data) =>{
+        setCriptos(data.data)
+      })
+      .catch(() => {
+        console.error("La petición falló")
+      })
+  },[])
+
+  if(!criptos) return <span>Cargando...</span>
+
   return (
-    <h1>Hola</h1>
+    <>
+      <h1>Criptos</h1>
+
+      <ul>
+        {criptos.map(({id, name,priceUsd}) => (
+          <li key={id}>Nombre: {name} Precio: {priceUsd}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
